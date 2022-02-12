15b44c1ec0776d9ab0a9c7dd491da5371b91fccf
kt1120xxx <kt1120xxx@icloud.com>
2022-02-12 18:32:05 +0900

    カウンターロジック削除

--- nuxt_app/pages/index.vue
+++ nuxt_app/pages/index.vue
@@ -1,19 +1,9 @@
 <template>
   <section class="container">
-    <div>
-      <Counter></Counter>
-      <hr>
-      <Counter></Counter>
-      <hr>
-      <Counter></Counter>
-    </div>
+   
   </section>
 </template>
 <script>
-import Counter from '~/components/Counter.vue'
 export default {
-  components: {
-    Counter
-  }
 }
 </script>
